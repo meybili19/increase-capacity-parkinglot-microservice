d7285601c7ad8411fe1059c1d26ddc874ad5b037
Meybili <109104859+meybili19@users.noreply.github.com>
2025-02-13 09:22:46 -0500

    feat:

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const capacityRoutes = require('./routes/capacityRoutes');
 
 dotenv.config();
 
+
+
 const app = express();
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
 app.use(express.json());
 
 app.use('/capacity', capacityRoutes);
